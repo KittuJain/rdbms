3387cd9657af073b0c11ee6575be96cd6da2345f
Kittu Jain <cskratijain@gmail.com>
2015-03-06 16:48:01 +0530

    learned sub_queries inline view and 'having' keyword

---
+++ test_6_mar.sql
@@ -0,0 +1,24 @@
+-- create a system where people can apply for STEP program
+
+Candidate 
+	Name
+	Gender
+	Address
+	City
+	State
+	Email_Address
+	D.O.B.
+	Cell_phone
+	college
+	percentage
+	10th standard roll_number
+	Diploma Holder
+	Previously Applied(constraint of yes/no)
+
+Form Submission
+	Applicant ID
+	Name
+	Exam Center(Place)
+
+
+-- READ FUNCTION DECODE	-- 
